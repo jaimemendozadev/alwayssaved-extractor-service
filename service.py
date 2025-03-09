cd56700821d963386eb5e65e8398b56fa52fcf67
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-03-09 16:07:00 -0700

    Add delete_local_file function to audio_extractor service

--- service.py
+++ service.py
@@ -7,4 +7,7 @@ from services.audio_extractor.main import download_audio
 
 def run_extractor_service():
     # Example Usage:
-    download_audio("")
+    download_audio("https://www.youtube.com/watch?v=8Ve5SAFPYZ8")
+
+
+run_extractor_service()
--- services/audio_extractor/main.py
+++ services/audio_extractor/main.py
@@ -2,6 +2,7 @@
 audio_extractor service file
 """
 
+import os
 import re
 from typing import Any
 
@@ -11,6 +12,18 @@ import yt_dlp
 def sanitize_filename(filename: str) -> str:
     """Removes invalid filename characters."""
     return re.sub(r'[\\/*?:"<>|]', "", filename).strip()
+
+
+def delete_local_file(file_path: str):
+    """Deletes the local MP3 file after uploading to S3."""
+    abs_path = os.path.abspath(file_path)
+    print(f"🔎 Trying to delete: {abs_path}")
+
+    if os.path.exists(abs_path):
+        os.remove(abs_path)
+        print(f"🗑️ Deleted {abs_path}")
+    else:
+        print(f"⚠️ File not found: {abs_path}")
 
 
 def download_audio(youtube_url: str):
@@ -24,7 +37,7 @@ def download_audio(youtube_url: str):
         video_title = sanitize_filename(info.get("title", "unknown_video"))
 
     # Define output filename using video title
-    output_path = f"{video_title}.mp3"
+    output_path = f"{video_title}"
 
     # Download audio with correct filename
     ydl_opts = {
@@ -47,8 +60,10 @@ def download_audio(youtube_url: str):
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([youtube_url])
+
         print(f"✅ Audio downloaded successfully: {output_path}")
-        return output_path
+        delete_local_file(os.path.abspath(f"{output_path}.mp3"))
+
     except yt_dlp.DownloadError as e:
         print(f"❌ Error downloading audio: {e}")
         return None
