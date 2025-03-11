6762403339e784138804a7120939a8de5dfc268e
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-03-11 07:23:37 -0700

    Add try/except to transcribe_audio_file

--- services/audio_transcription/main.py
+++ services/audio_transcription/main.py
@@ -4,16 +4,21 @@ import whisper
 
 
 def transcribe_audio_file(video_title: str, whisper_model: whisper.model) -> str | None:
-    transcript_name = f"{video_title}.txt"
+    try:
+        transcript_name = f"{video_title}.txt"
 
-    mp3_file_name = f"{video_title}.mp3"
-    mp3_file_abs_path = os.path.abspath(mp3_file_name)
+        mp3_file_name = f"{video_title}.mp3"
+        mp3_file_abs_path = os.path.abspath(mp3_file_name)
 
-    result = whisper_model.transcribe(mp3_file_abs_path, fp16=False)
+        result = whisper_model.transcribe(mp3_file_abs_path, fp16=False)
 
-    with open(file=transcript_name, mode="w", encoding="utf-8") as file:
-        file.write(result["text"])
-        print(f"✅ Audio successfully transcribed: {video_title}")
-        return transcript_name
+        with open(file=transcript_name, mode="w", encoding="utf-8") as file:
+            file.write(result["text"])
+            print(f"✅ Audio successfully transcribed: {video_title}")
+            return transcript_name
 
-    return None
+        return None
+
+    except ValueError as e:
+        print(f"❌ Value Error: {e}")
+        return None
