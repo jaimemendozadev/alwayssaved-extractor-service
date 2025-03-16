ce9d04e8ade317ecaed2a72419186ef870c4d73d
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-03-15 19:02:47 -0700

    Use get_secret function in sqs file

--- services/aws/sqs.py
+++ services/aws/sqs.py
@@ -3,6 +3,8 @@ import os
 
 import boto3
 
+from services.aws.ssm import get_secret
+
 AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 
 sqs_client = boto3.client("sqs", region_name=AWS_REGION)
@@ -11,7 +13,7 @@ sqs_client = boto3.client("sqs", region_name=AWS_REGION)
 def send_sqs_message(s3_transcript_url: str):
     """Sends a message to the SQS queue indicating the transcript is ready."""
 
-    EXTRACTOR_PUSH_QUEUE_URL = os.getenv("EXTRACTOR_PUSH_QUEUE_URL")
+    EXTRACTOR_PUSH_QUEUE_URL = get_secret("EXTRACTOR_PUSH_QUEUE_URL")
 
     if not EXTRACTOR_PUSH_QUEUE_URL:
         print("⚠️ ERROR: SQS Queue URL not set!")
--- services/aws/ssm.py
+++ services/aws/ssm.py
@@ -7,7 +7,7 @@ AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
 ssm_client = boto3.client("ssm", region_name=AWS_REGION)
 
 
-def get_secret(param_name):
+def get_secret(param_name) -> str:
     """Fetches secret from AWS Parameter Store."""
 
     response = ssm_client.get_parameter(Name=param_name, WithDecryption=True)
