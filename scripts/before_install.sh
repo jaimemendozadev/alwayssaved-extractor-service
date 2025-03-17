8d6b8d2bd22b603174dd5fc91a2e579c8da49d56
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-03-17 12:44:20 -0700

    Update before_install.sh to use correct param name

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,7 +8,7 @@ echo "Removing old application files..."
 sudo rm -rf /home/ec2-user/notecasts-extractor-service
 
 echo "Retrieving GitHub credentials from AWS Systems Manager..."
-GITHUB_TOKEN=$(aws ssm get-parameter --name "/notecasts/github_token" --with-decryption --query "Parameter.Value" --output text)
+GITHUB_TOKEN=$(aws ssm get-parameter --name "/notecasts/github_pat" --with-decryption --query "Parameter.Value" --output text)
 
 echo "Cloning the private repository..."
 git clone https://$GITHUB_TOKEN@github.com/jaimemendozadev/notecasts-extractor-service.git /home/ec2-user/notecasts-extractor-service
