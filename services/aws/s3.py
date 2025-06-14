dfd891b22bfe1fa97bebff036b7a077ef9980963
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-06-13 18:58:31 -0700

    Update upload_s3_file_record_in_db
    Had to perform an update to the function where we
    create a File document, get the new File document's
    _id as part of the s3_key for the s3 File upload,
    and then re-update the same File document with the
    new s3_key in the database.

--- services/aws/s3.py
+++ services/aws/s3.py
@@ -2,20 +2,21 @@ import os
 
 import boto3
 import boto3.exceptions
+from bson.objectid import ObjectId
 from pymongo import AsyncMongoClient
 
 from services.aws.ssm import get_secret
 from services.utils.types.main import BaseFilePayload, FilePayload
 
 
-# TODO Need to create a new File document with s3_key in the following format: /{fileOwner}/{noteID}/{fileID}/{fileName}.{fileExtension}
 async def upload_s3_file_record_in_db(
     s3_client: boto3.client,
     mongo_client: AsyncMongoClient,
     base_file_payload: BaseFilePayload,
 ) -> FilePayload:
     """
-    base_s3_key is "{user_id}/{note_id}"
+    Creates a new MongoDB File, uploads the File to s3, then updates the newly created File document with the s3_key.
+
     file_name is the media file name with .extension
     """
 
@@ -36,30 +37,45 @@ async def upload_s3_file_record_in_db(
 
         file_abs_path = os.path.abspath(file_name)
 
-        target_s3_key = f"{base_s3_key}/{file_name}"
+        if not os.path.exists(file_abs_path):
+            raise FileNotFoundError(f"File does not exist: {file_abs_path}")
 
-        _, file_extension = os.path.splitext(target_s3_key)
-
-        s3_client.upload_file(file_abs_path, bucket_name, target_s3_key)
+        _, file_extension = os.path.splitext(file_name)
 
         new_file_payload = {
             "user_id": user_id,
             "note_id": note_id,
-            "s3_key": target_s3_key,
             "file_name": file_name,
             "file_type": file_extension,
         }
 
+        insert_result = (
+            await mongo_client.get_database("alwayssaved")
+            .get_collection("files")
+            .insert_one(new_file_payload)
+        )
+
+        new_file_id = insert_result.inserted_id
+
+        target_s3_key = f"{base_s3_key}/{new_file_id}/{file_name}"
+
+        s3_client.upload_file(file_abs_path, bucket_name, target_s3_key)
+
         await mongo_client.get_database("alwayssaved").get_collection(
             "files"
-        ).insert_one(new_file_payload)
+        ).find_one_and_update(
+            {"_id": ObjectId(new_file_id)}, {"$set": {"s3_key": target_s3_key}}
+        )
 
         return {"s3_key": target_s3_key}
 
     except boto3.exceptions.S3UploadFailedError as e:
         print(f"❌ Error uploading file to s3 in upload_to_s3: {e}")
-        return {"s3_key": ""}
+
+    except FileNotFoundError as e:
+        print(f"❌ File not found: {e}")
 
     except ValueError as e:
         print(f"❌ Value Error in upload_to_s3: {e}")
-        return {"s3_key": ""}
+
+    return {"s3_key": ""}
