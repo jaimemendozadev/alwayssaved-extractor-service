f746269c779dda301402d673caa0b5c8eaf9210c
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2025-07-08 07:35:13 -0700

    Delete old version of delete_local_file

--- services/audio_extractor/main.py
+++ services/audio_extractor/main.py
@@ -12,20 +12,6 @@ from services.aws.ssm import get_secret
 s3_client = boto3.client("s3")
 
 
-# def delete_local_file(file_path: str):
-#     """Deletes the local MP3 file after uploading to S3."""
-#     abs_path = os.path.abspath(file_path)
-#     logging.info(f"🔎 Trying to delete: {abs_path}")
-
-#     try:
-#         os.remove(abs_path)
-#         logging.info(f"🗑️ Deleted {abs_path}")
-#     except FileNotFoundError:
-#         logging.warning(f"⚠️ File not found: {abs_path}")
-#     except Exception as e:
-#         logging.error(f"❌ Error deleting file {abs_path}: {e}")
-
-
 def delete_local_file(file_path: str):
     """Deletes the local MP3 file after uploading to S3."""
     try:
